7e5b3bd065e1ec7d17f3a7a16637d99823c2fc90
c1chy1 <36567964+c1chy1@users.noreply.github.com>
2019-03-12 22:32:41 +0100

    Add files via upload

---
+++ src/components/AddBookPanel.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import {updateBookAction} from '../store/actions'
+
+class AddBook extends React.Component {
+
+
+  addNewBook = (event) => {
+
+    event.preventDefault();
+
+    if (!this.props.editMode) {
+
+      const newBook = { ...this.props.book };
+
+      this.props.addNewBook(newBook);
+      // this.props.addBook(newBook);
+      this.props.updateBook({
+
+          name:'',
+          author: '',
+          description: '',
+          onStock: true,
+          image: '',
+          genre: '',
+          price: ''
+      })
+
+    } else {
+
+      const newBook = { ...this.props.book };
+      this.props.editBook(this.props.titleOfBookForRemoval, newBook);
+      this.setState({
+        book: {
+            name:'',
+            author: '',
+            description: '',
+            onStock: true,
+            image: '',
+            genre: '',
+            price: ''
+        }
+      })
+    }
+event.target.reset();
+
+  }
+
+  handleChange = (event) => {
+
+    let newBook;
+
+    if (event.target.name === "onStock") {
+      newBook = {
+        ...this.props.book,
+        [event.target.name]: event.target.checked,
+      };
+    } else {
+      newBook = {
+        ...this.props.book,
+        [event.target.name]: event.target.value
+      };
+    }
+   this.props.updateBook(newBook)
+
+  }
+
+  render () {
+
+    const label = this.props.editMode?"Edit":"Add";
+
+
+    return (
+      <div className="adminPanel col-xs-4">
+        <form onSubmit={this.addNewBook}>
+          <div className="form-group">
+            <input type="text" placeholder="Book name" id="name" name="name" className="form-control"
+                   onChange={this.handleChange} value={this.props.book.name}/>
+          </div>
+          <div className="form-group">
+            <input type="text" placeholder="Book author" id="author" name="author"
+                   className="form-control"
+                   onChange={this.handleChange} value={this.props.book.author}/>
+          </div>
+          <div className="form-group">
+                            <textarea placeholder="Book description" id="description" name="description"
+                                      className="form-control"
+                                      onChange={this.handleChange} value={this.props.book.description}/>
+          </div>
+
+          <div className="form-group">
+            <input type="text" placeholder="Book price" id="price" name="price"
+                   className="form-control"
+                   onChange={this.handleChange} value={this.props.book.price}/>
+          </div>
+          <select id="genre" className="select" name="genre" onChange={this.handleChange} value={this.props.book.genre}>
+            <option value="Drama">Drama</option>
+            <option  value="Comedy">Comedy</option>
+            <option  value="Thriller">Thriller</option>
+            <option value="Romantic">Romantic</option>
+            <option value="Horror">Horror</option>
+          </select>
+          <div className="form-group">
+            <input type="checkbox" id="onStock" name="onStock" className="form-check-input"
+                   onChange={this.handleChange} value={this.props.book.onStock}/>
+            <label htmlFor="onStock" className="form-check-label">On stock</label>
+          </div>
+          <div className="form-group">
+            <input type="text" placeholder="Book image" id="image" name="image" className="form-control"
+                   onChange={this.handleChange} value={this.props.book.image}/>
+          </div>
+          <button type="submit" className="btn btn-primary">{label}</button>
+
+        </form>
+          </div>
+
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+
+            return {
+              updateBook : book => dispatch(updateBookAction(book))
+            }
+
+}
+
+
+const mapStateToProps = state => {
+
+            return {
+              book: state.book,
+              editMode: state.editMode,
+              titleOfBookForRemoval: state.titleOfBookForRemoval
+            }
+}
+
+const AddBookPanel = connect(mapStateToProps,mapDispatchToProps)(AddBook)
+
+export default AddBookPanel;
---
+++ src/components/LoginPanel.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {firebaseApp} from '../fbase';
+
+
+export default class LoginPanel extends React.Component {
+
+ constructor() {
+   super();
+this.state = {
+  email: '',
+  password: ''
+}
+ }
+  authenticate = (event) => {
+    event.preventDefault();
+    firebaseApp.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+       this.props.changeLoggedIn(true);
+      })
+      .catch(() => {
+        console.log('Unable to authenticate');
+      })
+  }
+  handleLoginChange = (event) => {
+
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  }
+
+
+  render() {
+
+    return (
+
+      <form onSubmit={this.authenticate}>
+        <input type="text" placeholder="email" id="email_bs" name="email" className="form-control"
+               onChange={this.handleLoginChange} value={this.state.email}/>
+        <input type="password"  id="password_bs" name="password" className="form-control"
+               onChange={this.handleLoginChange} value={this.state.password}/>
+        <button type="submit" className="btn btn-primary">Log in</button>
+      </form>
+    )
+  }
+
+}
+
+
+

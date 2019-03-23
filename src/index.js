3b8f0fb2a9e61023809f993459245a3c2a67df14
c1chy1 <36567964+c1chy1@users.noreply.github.com>
2019-03-23 22:15:11 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Router from './components/Router';
 
 import './index.css';
+import './antd.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));

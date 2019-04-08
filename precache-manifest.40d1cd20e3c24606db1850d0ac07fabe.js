0d8e745159705c81cc614608262538b68df9224f
c1chy <cichy08081987pp@gmail.com>
2019-04-08 17:02:30 +0200

    Updates

---
+++ precache-manifest.40d1cd20e3c24606db1850d0ac07fabe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3e84e75878dc5d45b9e",
+    "url": "/bookstore/static/js/runtime~main.2e2d1535.js"
+  },
+  {
+    "revision": "7ff4de71e76034a2bc17",
+    "url": "/bookstore/static/js/main.71ea6e52.chunk.js"
+  },
+  {
+    "revision": "3ba3298e98738cda6579",
+    "url": "/bookstore/static/js/2.78bc4d31.chunk.js"
+  },
+  {
+    "revision": "7ff4de71e76034a2bc17",
+    "url": "/bookstore/static/css/main.658c3385.chunk.css"
+  },
+  {
+    "revision": "3ba3298e98738cda6579",
+    "url": "/bookstore/static/css/2.0121027a.chunk.css"
+  },
+  {
+    "revision": "948932628a7ed4e161802bd5068eee94",
+    "url": "/bookstore/index.html"
+  }
+];
